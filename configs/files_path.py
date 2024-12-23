0894c2e191b243d7b927d2948c93c65152808d16
Yoni <yonie3921@gmail.com>
2024-12-23 09:21:15 +0200

    Create folder with my ideas

--- configs/files_path.py
+++ configs/files_path.py
@@ -1,2 +1,3 @@
 csv_profile_path = '../csv_files/globalterrorismdb.csv'
 csv_performance_path = '../csv_files/Worldwide_Terrorism_Incidents_5000.csv'
+random_file = '../csv_files/RAND_Database_of_Worldwide_Terrorism_Incidents.csv'
