a2bfa9d2cfa63d9195ee5cbcdca183d0a05105df
Yoni <yonie3921@gmail.com>
2024-12-24 17:22:18 +0200

    Create calculation functions

--- db_service/calculation_services.py
+++ db_service/calculation_services.py
@@ -2,8 +2,6 @@ import folium
 from folium.plugins import MarkerCluster
 from db_repo.upload_to_pandas import upload_to_pandas
 
-df = upload_to_pandas()
-
 def calc_total_wounded(df):
     df['total_wounded_score'] = df['killed'] * 2 + df['wounded']
     df['total_wounded'] = df['killed'] + df['wounded']
@@ -14,4 +12,4 @@ def create_map_and_marker(df):
     folium_map = folium.Map(location=map_center, zoom_start=2)
     marker_cluster = MarkerCluster().add_to(folium_map)
 
-    return marker_cluster
+    return marker_cluster, folium_map
