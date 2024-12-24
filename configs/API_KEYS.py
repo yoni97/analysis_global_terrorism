ecb174f4fbd270957e01f28a308cd835a9c1b1a5
Yoni <yonie3921@gmail.com>
2024-12-24 09:11:22 +0200

    Apply api keys

---
+++ configs/API_KEYS.py
@@ -0,0 +1,6 @@
+API_KEY_NEWS = "0758b17b-67bc-4d79-851a-a8e3aa5b8545"
+NEWS_API_URL = "https://eventregistry.org/api/v1/article/getArticles"
+GROQ_API_URL = "https://api.groq.com/v1/classify"
+GROQ_API_KEY = "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"
+OPENCAGE_API_URL = "https://api.opencagedata.com/geocode/v1/json"
+OPENCAGE_API_KEY = "98cfcbbd4fb849719453a3e81dcfdb83"
