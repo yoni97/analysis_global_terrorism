bc186d82b7b334ace8cfcec9fb17dcd639ec929f
Yoni <yonie3921@gmail.com>
2024-12-24 17:25:20 +0200

    Improve upload to pandas function

--- db_repo/upload_to_pandas.py
+++ db_repo/upload_to_pandas.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from configs.mongodb import terrorism_actions
 
-def upload_to_pandas():
+def upload_to_pandas(collection):
     try:
-        results = list(terrorism_actions.find({}))
+        results = list(collection.find({}))
         df = pd.DataFrame(results)
     except Exception as e:
         return 'Could not connect to database or to convert to pandas dataframe'
