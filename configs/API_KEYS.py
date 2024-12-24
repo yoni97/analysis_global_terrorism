79862a274450736c12f1c8399520a41c9cd3bb3e
Yoni <yonie3921@gmail.com>
2024-12-24 17:20:49 +0200

    Creating api keys

--- configs/API_KEYS.py
+++ configs/API_KEYS.py
@@ -4,3 +4,4 @@ GROQ_API_URL = "https://api.groq.com/v1/classify"
 GROQ_API_KEY = "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"
 OPENCAGE_API_URL = "https://api.opencagedata.com/geocode/v1/json"
 OPENCAGE_API_KEY = "98cfcbbd4fb849719453a3e81dcfdb83"
+ELASTIC_API_KEY ='LWtjWC1aTUJvNTJ0MHZJX3RNZzc6ODgwV20zZHhRQ0dhLUFYY3pKRnV2QQ=='
