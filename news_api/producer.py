adb3f9aea40257c84ad479ac3e6193a07cef3273
Yoni <yonie3921@gmail.com>
2024-12-24 09:17:01 +0200

    Create kafka producer

---
+++ news_api/producer.py
@@ -0,0 +1,60 @@
+from kafka import KafkaProducer
+import json
+import time
+import requests
+from configs.API_KEYS import API_KEY_NEWS, NEWS_API_URL
+from configs.kafka_config import KAFKA_BROKER, KAFKA_TOPIC
+
+ARTICLES_PAGE = 1
+
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BROKER,
+    value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+def fetch_news():
+    global ARTICLES_PAGE
+    payload = {
+        "action": "getArticles",
+        "keyword": "terror attack",
+        "ignoreSourceGroupUri": "paywall/paywalled_sources",
+        "articlesPage": ARTICLES_PAGE,
+        "articlesCount": 1,
+        "articlesSortBy": "socialScore",
+        "articlesSortByAsc": False,
+        "dataType": ["news", "pr"],
+        "forceMaxDataTimeWindow": 31,
+        "resultType": "articles",
+        "apiKey": API_KEY_NEWS
+    }
+
+    response = requests.post(NEWS_API_URL, json=payload)
+    if response.status_code == 200:
+        try:
+            data = response.json()  # ניסיון לחלץ את ה-JSON
+            print(json.dumps(data, indent=4))  # הדפסת התגובה בצורה נוחה לקריאה
+            ARTICLES_PAGE += 1
+            return data.get("articles", {}).get("results", [])
+        except ValueError as e:
+            print(f"Error parsing JSON: {e}")
+            return []
+    else:
+        print(f"Failed to fetch news: {response.status_code}")
+        return []
+
+def send_to_kafka():
+    articles = fetch_news()
+    for article in articles:
+        if isinstance(article, dict) and 'title' in article:
+            print(f"Sent article: {article['title']} to Kafka")
+            producer.send(KAFKA_TOPIC, article)
+            print(article)
+            print(f"Sent article: {article['title']} to Kafka")
+        else:
+            print(f"Article is not a dictionary or does not contain 'title': {article}")
+    producer.flush()
+    producer.close()
+
+if __name__ == "__main__":
+    while True:
+        send_to_kafka()
+        time.sleep(120)
