b6d2f61497e6e50c59de0f3991f169865d034dd6
Yoni <yonie3921@gmail.com>
2024-12-24 09:13:40 +0200

    Returning none if there is not a field in the Json

--- db_repo/insert_to_mongo.py
+++ db_repo/insert_to_mongo.py
@@ -1,7 +1,6 @@
 import csv
 from datetime import datetime
 from configs.mongodb import terrorism_actions
-from configs.files_path import random_file
 
 def read_csv(file_path):
     with open(file_path, mode='r', encoding='utf-8') as file:
@@ -15,7 +14,7 @@ def convert_date(date_str):
     except ValueError:
         return None
 
-def init_terror_actions_big_data():
+def create_terror_actions_collection(df):
    terrorism_actions.drop()
    print('table dropped successfully')
    terror_list = []
@@ -23,13 +22,26 @@ def init_terror_actions_big_data():
    for row in read_csv('../pandas_analytics/normalized_data.csv'):
 
        new_terror = {
-           'eventid': row['eventid'], 'year': row['Year'], 'month': row['Month'], 'day': row['Day'],
-           'country': row['Country'], 'region': row['Region'], 'city': row['city'],
-           'latitude': row['latitude'], 'longitude': row['longitude'], 'AttackType': row['AttackType'],
-           'Group': row['Group'],'killed': row['Killed'], 'wounded': row['Wounded'], 'target': row['Target'],
-           'summary': row['Summary'], 'success': row['success'], 'target_type': row['Target_type'],
-           'total_wounded_score': row['severity'],'weapon_type': row['Weapon_type'],
-           'date': row['Date'], 'summary': row['Summary'],
+           'eventid': row.get('eventid', None),
+           'year': row.get('Year', None),
+           'month': row.get('Month', None),
+           'day': row.get('Day', None),
+           'country': row.get('Country', None),
+           'region': row.get('Region', None),
+           'city': row.get('city', None),
+           'date': row.get('Date', None),
+           'latitude': row.get('latitude', None),
+           'longitude': row.get('longitude', None),
+           'AttackType': row.get('AttackType', None),
+           'Group': row.get('Group', None),
+           'killed': row.get('Killed', None),
+           'wounded': row.get('Wounded', None),
+           'target': row.get('Target', None),
+           'summary': row.get('Summary', None),
+           'success': row.get('success', None),
+           'target_type': row.get('Target_type', None),
+           'total_wounded_score': row.get('severity', None),
+           'weapon_type': row.get('Weapon_type', None),
        }
        terror_list.append(new_terror)
 
@@ -42,56 +54,5 @@ def init_terror_actions_big_data():
    terrorism_actions.create_index('region')
    terrorism_actions.create_index('country')
 
-
-
-
-
-
-
-
-# with open(random_file, mode='r', encoding='latin1') as file:
-#     reader = csv.DictReader(file)
-#
-#     for row in reader:
-#         # המרת התאריך והפרדתו לשנה, חודש ויום
-#         date = convert_date(row['date'])
-#         if date:
-#             year = date.year
-#             month = date.month
-#             day = date.day
-#         else:
-#             year = month = day = None  # אם לא ניתן להמיר את התאריך
-#
-#         # יצירת השדות החדשים
-#         new_row = {
-#             'eventid': row['eventid'],
-#             'year': year,
-#             'month': month,
-#             'day': day,
-#             'country': row['country'],
-#             'city': row['city'],
-#             'Group': row['perpetrator'],
-#             'killed': row['fatalities'],
-#             'wounded': row['injuries'],
-#             'weapon_type': row['weapon'],
-#             'summary': row['description']
-#         }
-#
-#         # הוספת השורה לאוסף במונגוDB
-#         collection.insert_one(new_row)
-#
-# print("הנתונים נכתבו בהצלחה למונגוDB")
-
-# def init_new_terror_actions():
-#     terror_list = []
-#     for row in read_csv('../csv_files/RAND_Database_of_Worldwide_Terrorism_Incidents.csv'):
-#         new_terror_event = {}
-
-
-# # הוספת eventid כ-_id למסמכים
-# data_dict = data.to_dict(orient='records')  # המרת ה-DataFrame למילון של רשומות
-# for record in data_dict:
-#     record['_id'] = record['eventid']
-
 if __name__ == '__main__':
-    init_terror_actions_big_data()
+    create_terror_actions_collection()
